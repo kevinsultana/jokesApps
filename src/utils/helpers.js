c3bd0065cfa4c3ec09c903a4f7bc74853ab35ca4
kevinsultana <kevinpdg.ks@gmail.com>
2025-04-10 19:40:49 +0700

    Update ReadMe.md

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,6 +1,3 @@
 export const moveItemToTop = (list, item) => {
   return [item, ...list.filter(i => i !== item)];
 };
-
-export const generateKey = (prefix = '') =>
-  `${prefix}_${Math.random().toString(36).substring(2, 9)}`;
